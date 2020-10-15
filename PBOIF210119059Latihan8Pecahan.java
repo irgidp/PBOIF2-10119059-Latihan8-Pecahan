96dc25947810e7523943724ef3c4daa4f76f07f0
irgidp <irgijak@gmail.com>
2020-10-15 22:16:21 +0700

    Latihan 8

---
+++ PBOIF210119059Latihan8Pecahan.java
@@ -0,0 +1,30 @@
+/* 
+* @author
+ * NAMA        : Irgi Dwiputra
+ * KELAS       : IF-2
+ * NIM         : 10119059
+ * Deskripsi   : Program ini berisi program untuk menampilkan penulisan pecahan
+ */
+package pboif2.pkg10119059.latihan8.pecahan;
+
+/**
+ *
+ * @author Corazon
+ */
+public class PBOIF210119059Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double: " + d);
+    }
+    
+}
